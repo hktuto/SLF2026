9af05c5763afa92e6e087b3a96a7cc3d28599462
Sean Tsang <9394wing@gmail.com>
2026-04-29 10:39:21 +0800

    Update index.vue

--- apps/hkrw2026/pages/book2/index.vue
+++ apps/hkrw2026/pages/book2/index.vue
@@ -141,7 +141,7 @@ onMounted(() => {
 
              <div class="btns">
                <ElButton v-if="book.book_link_HK" type="info" @click="openBooks(tObj('book_link_', book))">{{t("bookLink")}}</ElButton>
-               <ElButton v-if="book.ebook_link_hk" type="info" @click="openBooks(tObj('ebook_link_', book))">{{t("eBookLink")}}</ElButton>
+               <ElButton v-if="book.eBook_link_hk" type="info" @click="openBooks(tObj('eBook_link_', book))">{{t("eBookLink")}}</ElButton>
              </div>
               <div class="author">{{t('publisher')}} : {{tObj('publisher_', book)}}</div>
               <div class="author">{{t('publishYear')}} : {{book.year_HK}}</div>
